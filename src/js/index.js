8efdb0493b9ef1574c916034ffd3997d7b3410b4
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-17 12:04:40 -0300

    refatorado apos correção

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,56 @@
-const botaoEnviar = document.getElementById("form");
-const estadoDePreenchimento = document.querySelectorAll(".estado-inicial");
-const campoObrigatorio = document.querySelectorAll(".spanNone");
+// primeira versão
+
+// const botaoEnviar = document.getElementById("form");
+// const estadoDePreenchimento = document.querySelectorAll(".estado-inicial");
+// const campoObrigatorio = document.querySelectorAll(".spanNone");
 
 
-botaoEnviar.addEventListener("submit", function (event){
-    event.preventDefault();
+// botaoEnviar.addEventListener("submit", function (event){
+//     event.preventDefault();
     
 
-    estadoDePreenchimento.forEach((campo , index) => {
+//     estadoDePreenchimento.forEach((campo , index) => {
 
-        resetarVerificacaoFormulario(campo, index);
+//         resetarVerificacaoFormulario(campo, index);
 
-        if (campo.value === "") {
-            campo.classList.add("preenchido-não");
+//         if (campo.value === "") {
+//             campo.classList.add("preenchido-nao");
             
-        }else {
-            campo.classList.add("preenchido-sim");
-            removerCampoObrigatorio(index);
+//         }else {
+//             campo.classList.add("preenchido-sim");
+//             removerCampoObrigatorio(index);
             
+//         }
+//     })
+// })
+
+// function removerCampoObrigatorio(index) {
+//     campoObrigatorio[index].classList.replace("spaninline", "spanNone");
+// }
+
+// function resetarVerificacaoFormulario(campo, index) {
+//     campo.classList.remove("preenchido-nao", "preenchido-sim");
+//     campoObrigatorio[index].classList.replace("spanNone", "spaninline");
+// }
+
+/* Codigo refatorado apos correção */
+
+const estadoDePreenchimento = document.querySelectorAll(".estado-inicial");
+const botaoEnviar = document.querySelector(".enviar");
+
+botaoEnviar.addEventListener("click", (e) =>{
+    e.preventDefault()
+
+    estadoDePreenchimento.forEach((input)=>{
+        if (input.value) {
+            input.classList.add("preenchido-sim");
+            input.classList.remove("preenchido-nao");
+            input.nextElementSibling.classList.remove("spaninline")
+            
+        } else {
+            input.classList.remove("preenchido-sim");
+            input.classList.add("preenchido-nao");
+            input.nextElementSibling.classList.add("spaninline")
         }
     })
 })
-
-function removerCampoObrigatorio(index) {
-    campoObrigatorio[index].classList.replace("spaninline", "spanNone");
-}
-
-function resetarVerificacaoFormulario(campo, index) {
-    campo.classList.remove("preenchido-não", "preenchido-sim");
-    campoObrigatorio[index].classList.replace("spanNone", "spaninline");
-}
